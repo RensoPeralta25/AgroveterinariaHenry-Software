bb529e202ec63fc069ae478e965d38975d810583
Adrian Cruz <AXCS0005@ce.pucmm.edu.do>
2026-05-19 22:56:52 -0400

    feat(AHS-10): creacion del view para el crud de usuario

---
+++ src/main/java/com/agroveterinaria/view/UsuarioView.java
@@ -0,0 +1,28 @@
+package com.agroveterinaria.view;
+
+import com.agroveterinaria.entity.Usuario;
+import com.agroveterinaria.service.UsuarioService;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.router.Route;
+import org.vaadin.crudui.crud.impl.GridCrud;
+
+
+@Route("usuarios")
+public class UsuarioView extends VerticalLayout {
+
+    public UsuarioView(UsuarioService usuarioService) {
+        GridCrud<Usuario> crudUsuario = new GridCrud<>(Usuario.class);
+        crudUsuario.getGrid().setColumns("idUsuario", "username");
+        crudUsuario.getGrid().getColumnByKey("idUsuario").setHeader("ID");
+        crudUsuario.getGrid().getColumnByKey("username").setHeader("Usuario");
+
+        crudUsuario.getCrudFormFactory().setVisibleProperties("username", "password");
+        crudUsuario.getCrudFormFactory().setFieldType("password", PasswordField.class);
+
+        crudUsuario.setFindAllOperation(usuarioService::findAll);
+        crudUsuario.setAddOperation(usuarioService::add);
+
+        add(crudUsuario);
+    }
+}
