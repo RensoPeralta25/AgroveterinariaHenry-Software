b0e6f9a4dea70c2bd397b69fd86ba6511c6c239a
jmcom <jmca0001@ce.pucmm.edu.do>
2026-05-27 23:28:33 -0400

    feat(AHS-46): atributo de estado añadido a producto y gestionado en el CRUD

--- src/main/java/com/agroveterinaria/enums/StatusEntidad.java
+++ src/main/java/com/agroveterinaria/enums/StatusEntidad.java
@@ -1,5 +1,16 @@
 package com.agroveterinaria.enums;
 
 public enum StatusEntidad {
-    ACTIVO, INACTIVO
+    ACTIVO("Activo"),
+    INACTIVO("Inactivo");
+
+    private final String etiqueta;
+
+    StatusEntidad(String etiqueta) {
+        this.etiqueta = etiqueta;
+    }
+
+    public String getEtiqueta() {
+        return etiqueta;
+    }
 }
