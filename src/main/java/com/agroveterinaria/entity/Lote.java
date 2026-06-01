a3145ea1229ee5fb05051689af7b5369ac012163
jmcom <jmca0001@ce.pucmm.edu.do>
2026-05-31 23:15:14 -0400

    feat(AHS-50): persistencia de almacén, lote, e Inventaria lista, en teoría

---
+++ src/main/java/com/agroveterinaria/entity/Lote.java
@@ -0,0 +1,30 @@
+package com.agroveterinaria.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "lote")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Lote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_lote")
+    private Long idLote;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_producto", nullable = false)
+    private Producto producto;
+
+    @Column(name = "fecha_vencimiento")
+    private LocalDate fechaVencimiento;
+}
