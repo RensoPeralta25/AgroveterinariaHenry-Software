810a29601df55a678087f09bbeccb5e81bee5ec8
Adrian Cruz <AXCS0005@ce.pucmm.edu.do>
2026-06-04 15:35:11 -0400

    feat(AHS-26): creacion de clases para seguridad

---
+++ src/main/java/com/agroveterinaria/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.agroveterinaria.security;
+
+import com.agroveterinaria.view.login.LoginView;
+import com.vaadin.flow.spring.security.VaadinSecurityConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true)
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.with(VaadinSecurityConfigurer.vaadin(), configurer -> {
+           configurer.loginView(LoginView.class);
+        });
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
---
+++ src/main/java/com/agroveterinaria/security/UserDetailsServiceImpl.java
@@ -0,0 +1,47 @@
+package com.agroveterinaria.security;
+
+import com.agroveterinaria.entity.Empleado;
+import com.agroveterinaria.entity.Usuario;
+import com.agroveterinaria.repository.EmpleadoRepository;
+import com.agroveterinaria.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UsuarioRepository usuarioRepository;
+    private final EmpleadoRepository empleadoRepository;
+
+    public UserDetailsServiceImpl(UsuarioRepository usuarioRepository, EmpleadoRepository empleadoRepository) {
+        this.usuarioRepository = usuarioRepository;
+        this.empleadoRepository = empleadoRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+
+        Optional<Empleado> emp = empleadoRepository.findByUsuario(usuario);
+
+        if (emp.isEmpty() || emp.get().getCargos().isEmpty()) {
+            throw new UsernameNotFoundException("Acceso denegado: El usuario no tiene un empleado o roles válidos asignados.");
+        }
+
+        String[] roles = emp.get().getCargos().stream()
+                .map(Enum::name)
+                .toArray(String[]::new);
+
+        return User.withUsername(usuario.getUsername())
+                .password(usuario.getPassword())
+                .roles(roles)
+                .build();
+    }
+}
