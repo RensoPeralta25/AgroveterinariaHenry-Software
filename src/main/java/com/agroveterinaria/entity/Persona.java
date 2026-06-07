11200cd288af1400127a826cef44ac54b9ef092d
Adrian Cruz <AXCS0005@ce.pucmm.edu.do>
2026-06-07 18:59:20 -0400

    feat(AHS-48): cambiando atributos de persona a privado

--- src/main/java/com/agroveterinaria/entity/Persona.java
+++ src/main/java/com/agroveterinaria/entity/Persona.java
@@ -22,16 +22,16 @@ public class Persona {
     private Long idPersona;
 
     @NotBlank(message = "El nombre no puede estar vacío")
-    protected String nombre;
+    private String nombre;
 
     @NotBlank(message = "La cédula es obligatoria")
     @Pattern(regexp = "^\\d{3}-\\d{7}-\\d{1}$", message = "El formato debe ser 000-0000000-0")
-    protected String cedula;
+    private String cedula;
 
     @NotBlank(message = "La dirección es obligatoria")
-    protected String direccion;
+    private String direccion;
 
     @NotBlank(message = "El teléfono es obligatorio")
     @Pattern(regexp = "^\\d{3}-\\d{3}-\\d{4}$", message = "El formato debe ser 000-000-0000")
-    protected String telefono;
+    private String telefono;
 }
