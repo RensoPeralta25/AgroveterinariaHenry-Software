4555b51b4b4161236a7f183d196885f0be71c5ef
Renso Peralta <ripu0001@ce.pucmm.edu.do>
2026-08-03 19:43:11 -0400

    conflictos con rama que ya estaba merge

--- src/main/resources/db/migration/V62__agregar_estado_a_vacaciones.sql
+++ src/main/resources/db/migration/V62__agregar_estado_a_vacaciones.sql
@@ -1,5 +1,11 @@
 ALTER TABLE vacacion_empleado ADD COLUMN estado VARCHAR(50);
 
+UPDATE vacacion_empleado
+SET estado = CASE
+    WHEN pagado IS TRUE THEN 'PAGADA'
+    ELSE 'APROBADA'
+END;
+
 ALTER TABLE vacacion_empleado ALTER COLUMN estado SET NOT NULL;
 
 ALTER TABLE vacacion_empleado ALTER COLUMN id_empleado_aprobador DROP NOT NULL;
